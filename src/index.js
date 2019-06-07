52f81c7337fac1a9367540cf098529b462016907
Jonathan Dai <jonathandai1226@gmail.com>
2019-06-07 01:09:30 -0500

    FIRST COMMIT

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+fetch('./data/products.json')
+.then(resp => resp.json())
+.then(({products}) => {
+   ReactDOM.render(<App products={products} />, document.getElementById('root'));
+});
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
